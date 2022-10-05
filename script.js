c7fafaeadca7dd0dcfb4e469e3c26fda6e083a79
PaulReaver <35564306+PaulReaver@users.noreply.github.com>
2022-10-05 21:42:28 +0300

    Added button that receives a number to change grid size

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 //Access the div container
 const CONTAINER = document.querySelector(".container");
 
-//Create 256 boxes in 16 rows and 16 columns
-for (let i = 1; i <= 256; i++) {
+//Default squares per side
+let squaresPerSide = 16;
+
+//Creates grid based and changes color of cells
+for (let i = 1; i <= Math.pow(squaresPerSide, 2); i++) {
     let CELL = document.createElement("div");
     CELL.addEventListener("mouseover", () => {
         CELL.style.backgroundColor = "black";
     }); 
     CONTAINER.appendChild(CELL);
 }
+
+//Asks for a number of cells per side to create new grid
+document.querySelector("button").addEventListener("click",() => {
+    do {
+        squaresPerSide = prompt ("Squares per side. Max number is 100.");
+        squaresPerSide = Number(squaresPerSide);     
+    } while (squaresPerSide > 100);
+    
+})
