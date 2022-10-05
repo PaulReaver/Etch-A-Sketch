4e1bea5bb68e4b33e42de08bbbe3fe7f74ebaedf
PaulReaver <35564306+PaulReaver@users.noreply.github.com>
2022-10-05 21:01:20 +0300

    Cells change color on hover

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 //Access the div container
-const container = document.querySelector(".container");
+const CONTAINER = document.querySelector(".container");
 
 //Create 256 boxes in 16 rows and 16 columns
 for (let i = 1; i <= 256; i++) {
-    const div = document.createElement("div");
-    container.appendChild(div);
-    
+    let CELL = document.createElement("div");
+    CELL.addEventListener("mouseover", () => {
+        CELL.style.backgroundColor = "black";
+    }); 
+    CONTAINER.appendChild(CELL);
 }
