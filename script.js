abe779ddab523d788805754678870e62840998fb
Paul Vallas <35564306+PaulReaver@users.noreply.github.com>
2022-12-19 13:01:53 +0200

    Formatted code

--- script.js
+++ script.js
@@ -14,22 +14,25 @@ const COLORRANDOM = document.querySelector('#color-random');
 COLORBLACK.style.transform = 'scale(1.5, 1.5)';
 COLORBLACK.style.border = '3px grey solid';
 
+// Paint function
+function paint() {
+    if (modeBlack) {
+        this.style.backgroundColor = 'black';
+        this.style.border = '1px black solid';
+    } else if (modeRandom) {
+        const randomColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        this.style.backgroundColor = randomColor;
+        this.style.border = `1px ${randomColor} solid`;
+    }
+}
+
 // Creates grid based and changes color of cells
 function createGrid() {
     for (let i = 1; i <= squaresPerSide ** 2; i += 1) {
         const CELL = document.createElement('div');
         CELL.style.backgroundColor = 'white';
         CELL.style.border = '1px lightGrey solid';
-        CELL.addEventListener('mouseover', () => {
-            if (modeBlack) {
-                CELL.style.backgroundColor = 'black';
-                CELL.style.border = '1px black solid';
-            } else if (modeRandom) {
-                const randomColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-                CELL.style.backgroundColor = randomColor;
-                CELL.style.border = `1px ${randomColor} solid`;
-            }
-        });
+        CELL.addEventListener('mouseover', paint);
         const calculation = 800 / squaresPerSide;
         CELL.style.width = `${calculation}px`;
         CELL.style.height = `${calculation}px`;
